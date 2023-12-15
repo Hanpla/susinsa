d12a24a3fccc83a7365474a8a08b0183bf191fc2
Hanpla <plasma306@naver.com>
2023-12-15 17:14:03 +0900

    first commit

---
+++ src/Pages/Shop.jsx
@@ -0,0 +1,17 @@
+import Hero from "../Components/Hero/Hero";
+import NewCollections from "../Components/NewCollections/NewCollections";
+import Offers from "../Components/Offers/Offers";
+import Popular from "../Components/Popular/Popular";
+
+const Shop = () => {
+  return (
+    <div>
+      <Hero />
+      <Popular />
+      <Offers />
+      <NewCollections />
+    </div>
+  );
+};
+
+export default Shop;
