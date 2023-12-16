0fa72f9246f6fa109ce669412c38b337d841bc78
Hanpla <plasma306@naver.com>
2023-12-16 16:54:45 +0900

    카테고리까지 수정

--- src/App.js
+++ src/App.js
@@ -5,6 +5,10 @@ import ShopCategory from "./Pages/ShopCategory";
 import Product from "./Pages/Product";
 import Cart from "./Pages/Cart";
 import LoginSignup from "./Pages/LoginSignup";
+import Footer from "./Components/Footer/Footer";
+import men_banner from "./Assets/banner_mens.png";
+import women_banner from "./Assets/banner_women.png";
+import kids_banner from "./Assets/banner_kids.png";
 
 function App() {
   return (
@@ -17,15 +21,25 @@ function App() {
             path="/"
             element={<Shop />}
           />
-          <Route path="/men" element={<ShopCategory category="men" />} />
-          <Route path="/women" element={<ShopCategory category="women" />} />
-          <Route path="/kids" element={<ShopCategory category="kids" />} />
+          <Route
+            path="/men"
+            element={<ShopCategory banner={men_banner} category="men" />}
+          />
+          <Route
+            path="/women"
+            element={<ShopCategory banner={women_banner} category="women" />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory banner={kids_banner} category="kid" />}
+          />
           <Route path="/product" element={<Product />}>
             <Route path="/productId" element={<Product />} />
           </Route>
           <Route path="/cart" element={<Cart />} />
           <Route path="/login" element={<LoginSignup />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </div>
   );
